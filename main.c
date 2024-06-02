2da05bb761e4359ffee8f3b58d63e5f697248114
fancy <ZhuofanWu@foxmail.com>
2024-06-02 16:50:05 +0800

    commit changes

--- main.c
+++ main.c
@@ -11,9 +11,10 @@
 #include <errno.h>
 #include <signal.h>
 
+#define PDU_SIZE 20
 int signal_flag=0;
 void handle_sigint();
-void server();
+void server(int connect_socket);
 
 int main(int argc, char *argv[]) {
     struct sigaction sa;
@@ -49,6 +50,21 @@ int main(int argc, char *argv[]) {
         struct sockaddr_in client_addr;
         socklen_t client_addrlen = sizeof(client_addr);
         int connect_socket = accept(listen_tcp_socket, (struct sockaddr *)&client_addr, &client_addrlen);
+        if (connect_socket < 0) {
+            if(errno==EINTR){
+                continue;
+            } else{
+                perror("Error in accept");
+                exit(1);
+            }
+        }
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+        int client_port = ntohs(client_addr.sin_port);
+        printf("[srv] client [%s:%d] is accepted!\n", client_ip, client_port);
+
+        server(connect_socket);
+
     }
 
 
@@ -60,6 +76,42 @@ void handle_sigint(){
     signal_flag=1;
 }
 
-void server(){
+void server(int connect_socket){
+    while (1){
+        char pdu[PDU_SIZE];
+        ssize_t bytes_read;
+        bytes_read = read(connect_socket, pdu, PDU_SIZE);
+        char temp_op[4],temp_op1[8],temp_op2[8]; //byte order?
+        strncpy(temp_op,pdu,4);
+        strncpy(temp_op1,pdu+4,8);
+        strncpy(temp_op2,pdu+4+8,8);
+        int32_t operator=atoi(temp_op);
+        int64_t operator_1= atoi(temp_op1);
+        int64_t operator_2= atoi(temp_op2);
+        int64_t result;
+        switch (operator) {
+            case 0x00000001:{
+                result = operator_1 + operator_2;
+                break;
+            }
+            case 0x00000002:{
+                result = operator_1 - operator_2;
+                break;
+            }
+            case 0x00000004:{
+                result = operator_1 * operator_2;
+                break;
+            }
+            case 0x00000008:{
+                result = operator_1 / operator_2;
+                break;
+            }
+            case 0x00000010:{
+                result = operator_1 % operator_2;
+                break;
+            }
+        }
 
+
+    }
 }
